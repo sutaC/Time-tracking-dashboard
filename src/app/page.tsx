1cd8329a3a500b8884dd98ecb3e7e229e37da861
Antoni Siekański <catus.pl@gmail.com>
2023-11-09 13:15:44 +0100

    Data fetch adj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import ProfileCard from "@/components/profileCard";
 import Card, { Category } from "@/components/card";
 import styles from "./page.module.css";
-import json from "@/../public/data/data.json";
+import json from "@/../public/documents/assets/data/data.json";
 
 export default async function Page() {
 	const data = json as Category[];
